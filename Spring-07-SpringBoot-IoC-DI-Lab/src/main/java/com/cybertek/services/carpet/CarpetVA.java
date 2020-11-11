d4192436ed52ed1ed5f4b970bacdf4a4ef715c49
abaiaz487 <baiazbekova@gmail.com>
2020-11-11 18:35:44 -0500

    calculator demo

--- Spring-07-SpringBoot-IoC-DI-Lab/src/main/java/com/cybertek/services/carpet/CarpetVA.java
+++ Spring-07-SpringBoot-IoC-DI-Lab/src/main/java/com/cybertek/services/carpet/CarpetVA.java
@@ -2,12 +2,14 @@ package com.cybertek.services.carpet;
 
 import com.cybertek.enums.City;
 import com.cybertek.interfaces.carpetPrices.Carpet;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+@Component
 public class CarpetVA implements Carpet {
 
     //dummy data that will run one time first time
@@ -24,18 +26,16 @@ public class CarpetVA implements Carpet {
     @Override
     public BigDecimal getSqFtPrice(City city) {
 
+        //if the city is not in the list, then return zero. logic for invalid data
+        BigDecimal defaultValue = BigDecimal.ZERO;
+
         //entrySet() converts map to set
         //below is stream
+        //Optional will return exception but it will say that there is no value. if there was no Optional, it would return null pointer exception
+        //when there is null pointer exception, you cant understand if the obj is not created or there is no value
        Optional<Map.Entry <City,BigDecimal>> collect = sqPriceForCity.entrySet().stream().filter(x -> x.getKey() == city).findFirst();
 
+       return collect.isPresent() ? collect.get().getValue() : defaultValue;
 
-
-
-
-
-
-
-
-        return null;
     }
 }
