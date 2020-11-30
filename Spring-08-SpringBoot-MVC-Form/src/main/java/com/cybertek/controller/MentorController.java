6f848a37f09cfe924db3e83c8f773c9563a79e6a
abaiaz487 <baiazbekova@gmail.com>
2020-11-30 10:54:18 -0500

    springboot demo

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -0,0 +1,28 @@
+package com.cybertek.controller;
+
+
+import com.cybertek.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ("/mentor")
+public class MentorController {
+
+
+    @GetMapping ("/register") //retrieving the form
+    public String showForm(Model model){
+
+        model.addAttribute("mentor",new Mentor());
+        return "/mentor/mentor-register";
+
+
+
+
+    }
+
+
+
+}
