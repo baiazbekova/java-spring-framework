1533e9e7d866c1429f7314a5222869c5520ecf35
abaiaz487 <baiazbekova@gmail.com>
2020-11-30 12:04:34 -0500

    mentor registration form

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -1,25 +1,25 @@
 package com.cybertek.controller;
-
-
 import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping ("/mentor")
 public class MentorController {
 
-
     @GetMapping ("/register") //retrieving the form
     public String showForm(Model model){
+        model.addAttribute("mentor", new Mentor());
 
-        model.addAttribute("mentor",new Mentor());
-        return "/mentor/mentor-register";
-
-
-
+        //creating collection
+        List<String> batchList = Arrays.asList("","B7","B8","B9","B10","B11","B12","E1","E2","E3","E4","E5");
+        model.addAttribute("batchList", batchList);
+        return "mentor/mentor-register";
 
     }
 
