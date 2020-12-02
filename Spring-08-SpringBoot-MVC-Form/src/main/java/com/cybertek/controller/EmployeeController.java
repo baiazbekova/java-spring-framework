767bb1cb475e846c4d5b5502538f77839990b11e
abaiaz487 <baiazbekova@gmail.com>
2020-12-02 07:35:19 -0500

    employee create

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.cybertek.controller;
 
 
+import com.cybertek.datagenerator.DataGenerator;
+import com.cybertek.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
     @GetMapping  ("/register")
-    public String employeeCreate(){
+    public String employeeCreate(Model model){
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getStateList());
+
         return "/templates/employee/employee-create";
     }
 }
